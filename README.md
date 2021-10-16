14436cff8c9fd44e885b7bc3f657c61e6f4b95ad
xiaoduan.KD <1961018000@qq.com>
2021-10-16 15:43:57 +0800

    feat:duan-cli vue模版第一次提交

---
+++ README.md
@@ -0,0 +1,20 @@
+# duan-vue-temp
+> 为duan-cli创建vue项目模版
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <div class="router">
+      <router-link
+        v-for="item in routesList"
+        :to="{ path: item.path }"
+        :key="item.path"
+        >{{ item.name }}</router-link
+      >
+    </div>
+    <img alt="Vue logo" src="./assets/img/logo.png" />
+    <HelloWorld msg="a duan vue template" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  data: function() {
+    return {
+      routesList: [],
+    };
+  },
+  created: function() {},
+  mounted: function() {
+    this.routesList = [...this.$router.options.routes];
+    console.log(this.routesList);
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.router > a {
+  margin-right: 10px;
+}
+</style>
